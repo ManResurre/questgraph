3097b52b9e16689ea0d881deebfdd801c55ff5dd
serge <korolariya2@gmail.com>
2025-07-06 15:11:24 +0300

    update dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        outputFileTracing: true,
+        optimizePackageImports: [
+            '@react-three/fiber',
+            'three',
+            'react-dnd',
+            'dnd-core',
+            '@radix-ui/react-icons'
+        ]
+    },
+    poweredByHeader: false,
+    generateEtags: false,
+    webpack: (config, {isServer}) => {
+        if (isServer) {
+            // Уменьшаем размер серверного бандла
+            config.optimization.splitChunks = false;
+            config.optimization.minimize = true;
+
+            // Исключаем тяжелые модули
+            config.externals = config.externals || [];
+            config.externals.push(({context, request}, callback) => {
+                if (/^@?(aws-sdk|sharp|bcrypt|canvas|puppeteer)/.test(request)) {
+                    return callback(null, `commonjs ${request}`);
+                }
+                callback();
+            });
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
